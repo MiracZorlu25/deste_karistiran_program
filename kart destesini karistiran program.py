061c3d7e193e821db1e128810e458e7419b0102e
Miraç ZORLU <miraczorlu2@gmail.com>
2023-09-06 18:18:20 +0300

    kart destesini karıştırır
    iskambil destesini karıştırmaya yarar

---
+++ kart destesini karistiran program.py
@@ -0,0 +1,13 @@
+ 
+# Bir kart destesini karıştırmak için Python programı
+#  modülleri içe aktarma
+import itertools, random
+# kart destesi oluştur 
+kart = list(itertools.product(range(1,14),['Maça', 'Kupa', 'Karo', 'Sinek']))
+# Kartları karıştır
+random.shuffle(kart)
+# Karıştırılmış kartları yazdır
+ 
+print("Kartlarınız:")
+for i in range(5):
+   print(kart[i][1]," ", kart[i][0] )
